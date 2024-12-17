099178eb75131d83e049043c0a3bd448f0bab7d5
Dieter Wachters <dieter@raito.io>
2024-12-17 13:55:06 +0100

    Adding org

--- .infra/infra/providers.tf
+++ .infra/infra/providers.tf
@@ -1,6 +1,7 @@
 provider "snowflake" {
   role                     = "ACCOUNTADMIN"
   account_name             = var.snowflake_account
+  organization_name        = var.snowflake_organization
   user                     = var.snowflake_user
   password                 = var.snowflake_password
   preview_features_enabled = ["snowflake_function_sql_resource"]
