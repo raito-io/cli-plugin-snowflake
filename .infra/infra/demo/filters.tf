10648d0155f014e5f26dff859798d33234f9fb00
Dieter Wachters <dieter@raito.io>
2024-12-17 14:14:36 +0100

    Testing fix1

--- .infra/infra/demo/filters.tf
+++ .infra/infra/demo/filters.tf
@@ -3,11 +3,12 @@ resource "snowflake_row_access_policy" "rls_sales_data" {
   database = snowflake_schema.sales.database
   schema   = snowflake_schema.sales.name
 
-  signature = {
-    TerritoryID = "NUMBER(38,0)"
+  argument = {
+    name = "TerritoryID"
+    type = "NUMBER(38,0)"
   }
 
-  row_access_expression = <<-EOF
+  body = <<-EOF
     current_role() in ('SALES_EXEC') or exists (
      select 1 from ${snowflake_table.sales_person.fully_qualified_name} WHERE
       current_role() in ('SALES') AND
