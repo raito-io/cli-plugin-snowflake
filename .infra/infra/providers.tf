c4dcf516b0afa6da7507387928ca012a7acf4d44
Dieter Wachters <dieter@raito.io>
2024-12-17 13:48:40 +0100

    fmt

--- .infra/infra/providers.tf
+++ .infra/infra/providers.tf
@@ -1,6 +1,6 @@
 provider "snowflake" {
   role                     = "ACCOUNTADMIN"
-  account_name                  = var.snowflake_account
+  account_name             = var.snowflake_account
   user                     = var.snowflake_user
   password                 = var.snowflake_password
   preview_features_enabled = ["snowflake_function_sql_resource"]
