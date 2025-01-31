0a649aa7afbed8f17e1efaa5dbb290504175a4f1
Ruben Mennes <ruben.mennes@me.com>
2025-01-31 16:45:42 +0100

    Filter on outbound shares

--- .infra/infra/destroy.go
+++ .infra/infra/destroy.go
@@ -115,9 +115,14 @@ func dropDatabaseRoles(db *sql.DB) error {
 }
 
 func dropOutboundShares(db *sql.DB, currentRole string) error {
-	rows, err := db.Query("SHOW SHARES")
+	_, err := db.Query("SHOW SHARES")
 	if err != nil {
 		return fmt.Errorf("query snowflake shares: %w", err)
+	}
+
+	rows, err := db.Query("select \"name\", \"owner\", \"to\" from table(result_scan(LAST_QUERY_ID())) WHERE \"kind\" = 'OUTBOUND'")
+	if err != nil {
+		return fmt.Errorf("query snowflake shares with filter: %w", err)
 	}
 
 	defer rows.Close()
